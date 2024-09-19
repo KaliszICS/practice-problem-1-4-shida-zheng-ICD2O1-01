a4ad9d13a8c6e606b67120864bbc8b3489aec2f4
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-09-19 13:31:12 +0000

    notes updated

--- Notes.py
+++ Notes.py
@@ -1,1 +1,29 @@
+'''
+    Lesson: Input and F strings
+    Author: Mr. Kalisz
+    Date Created: Sept 19, 2024
+    Date Last Modified: Spet 19, 2024
+'''
+
+#Input - the opposite of output
+
+# input() # Python will stop at this line of code and wait until the user types something and presses enter
+
+# Input needs to be stored and is always a String
+
+words = input() #stores the input into the variable words
+print(words) #Outputs what was saved by input
+
+# Prompt
+
+words = input("Enter your name: ") #Put your prompt inside the brackets
+print("Your name is " + words)
+
+#Review Concatentation
+
+
+
+#F Strings
+
+
 
