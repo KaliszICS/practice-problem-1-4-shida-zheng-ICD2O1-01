df04d8baf7c516d2ac53810098c80e6ab1327010
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-09-19 10:14:39 -0400

    Update PP1_4_test.py

--- PP1_4_test.py
+++ PP1_4_test.py
@@ -10,10 +10,15 @@ def test_q1_1(capsys):
   except:
     sys.exit()
 
+  input_values = ["word"]
+  
+  def mock_input(s):
+    print(s, end='')
+    return input_values.pop(0)
+  PP1_4.input = mock_input
+  
   q1()
   captured = capsys.readouterr()
-  sys.stderr.write("word\n")
-  captured += capsys.readouterr()
   assert captured.out == "Input a word: word\n"
 
 def test_q1_1(capsys):
@@ -24,10 +29,15 @@ def test_q1_1(capsys):
   except:
     sys.exit()
 
+  input_values = ["help"]
+  
+  def mock_input(s):
+    print(s, end='')
+    return input_values.pop(0)
+  PP1_4.input = mock_input
+  
   q1()
   captured = capsys.readouterr()
-  sys.stderr.write("help\n")
-  captured += capsys.readouterr()
   assert captured.out == "Input a word: help\n"
 
 def test_q2_1(capsys):
@@ -38,10 +48,15 @@ def test_q2_1(capsys):
   except:
     sys.exit()
 
+  input_values = ["George"]
+  
+  def mock_input(s):
+    print(s, end='')
+    return input_values.pop(0)
+  PP1_4.input = mock_input
+
   q2()
   captured = capsys.readouterr()
-  sys.stderr.write("George\n")
-  captured += capsys.readouterr()
   assert captured.out == "Input your first name: Hello George\n"
 
 def test_q2_2(capsys):
@@ -52,10 +67,15 @@ def test_q2_2(capsys):
   except:
     sys.exit()
 
+  input_values = ["Gretta"]
+  
+  def mock_input(s):
+    print(s, end='')
+    return input_values.pop(0)
+  PP1_4.input = mock_input
+
   q2()
   captured = capsys.readouterr()
-  sys.stderr.write("Gretta\n")
-  captured += capsys.readouterr()
   assert captured.out == "Input your first name: Hello Gretta\n"
 
 def test_q3_1(capsys):
@@ -66,11 +86,15 @@ def test_q3_1(capsys):
   except:
     sys.exit()
 
+  input_values = ["Alan", "Marr"]
+  
+  def mock_input(s):
+    print(s, end='')
+    return input_values.pop(0)
+  PP1_4.input = mock_input
+
   q3()
   captured = capsys.readouterr()
-  sys.stderr.write("Alan\n")
-  sys.stderr.write("Marr\n")
-  captured += capsys.readouterr()
   assert captured.out == "Input your first name: Input your last name: Marr Alan\n"
 
 def test_q3_2(capsys):
@@ -81,11 +105,15 @@ def test_q3_2(capsys):
   except:
     sys.exit()
 
+  input_values = ["James", "Kalisz"]
+  
+  def mock_input(s):
+    print(s, end='')
+    return input_values.pop(0)
+  PP1_4.input = mock_input
+
   q3()
   captured = capsys.readouterr()
-  sys.stderr.write("James\n")
-  sys.stderr.write("Kalisz\n")
-  captured += capsys.readouterr()
   assert captured.out == "Input your first name: Input your last name: Kalisz James\n"
 
 def test_q4_1(capsys):
@@ -96,11 +124,15 @@ def test_q4_1(capsys):
   except:
     sys.exit()
 
+  input_values = ["Fatima", "Wan Ling"]
+  
+  def mock_input(s):
+    print(s, end='')
+    return input_values.pop(0)
+  PP1_4.input = mock_input
+
   q4()
   captured = capsys.readouterr()
-  sys.stderr.write("Fatima\n")
-  sys.stderr.write("Wan Ling\n")
-  captured += capsys.readouterr()
   assert captured.out == "Input a student: Input another student: Your students are Fatima and Wan Ling\n"
 
 def test_q4_2(capsys):
@@ -111,9 +143,13 @@ def test_q4_2(capsys):
   except:
     sys.exit()
 
+  input_values = ["Kalie", "Steve"]
+  
+  def mock_input(s):
+    print(s, end='')
+    return input_values.pop(0)
+  PP1_4.input = mock_input
+
   q4()
   captured = capsys.readouterr()
-  sys.stderr.write("Kalie\n")
-  sys.stderr.write("Steve\n")
-  captured += capsys.readouterr()
   assert captured.out == "Input a student: Input another student: Your students are Kalie and Steve\n"
