d6722717e8f64b1d091f3606a91fca70b8cfebc1
shida-zheng-ICD2O1-01 <shida.zheng@student.tdsb.on.ca>
2024-11-07 16:37:19 +0000

    trry

--- PP1_4.py
+++ PP1_4.py
@@ -2,19 +2,26 @@
 
 def q1():
   #Write Assignment code here
-
+  word = input("Input a word: ")
+  print(word)
 def q2():
   #Write Assignment code here
-
+  first_name = input("Input your first name: ")
+  print("Hello " + first_name)
 def q3():
   #Write Assignment code here
-
+  first_name = input("Input your first name: ")
+  last_name = input("Input your last name: ")
+  print(last_name, first_name)
 def q4():
   #Write Assignment code here
+  student1 = input("Input a student: ")
+  student2 = input("Input another student: ")
+  print(f"Your students are {student1} and {student2}")
 
 #Do not edit code below this comment
 
-q1()
-q2()
-q3()
-q4()
+#q1()
+#q2()
+#q3()
+#q4()
